782b6bbcccc80c59ad9551df4e2e006efb8409fb
emanrdesu <eperezpozo@knights.ucf.edu>
2022-12-21 12:05:33 -0500

    Add initialization and wrap up main() code

--- c/pommy.c
+++ c/pommy.c
@@ -8,5 +8,16 @@
 // gcc -o pommy `pkg-config --cflags --libs libnotify ncurses` pommy.c
 
 int main() {
+  // ncurses initialization
+  initscr();
+  raw();
+  noecho();
+
+  // libnotify initialization
+  notify_init ("pommy");
+
+  // wrap up
+  endwin();        // ncurses
+  notify_uninit(); // libnotify
 	return 0;
 }
