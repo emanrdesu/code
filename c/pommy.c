a365a8c90147c1fdbf1967b9cd340024e0e2417e
emanrdesu <eperezpozo@knights.ucf.edu>
2022-12-21 13:26:30 -0500

    Add globals

--- c/pommy.c
+++ c/pommy.c
@@ -7,7 +7,26 @@
 // compile command:
 // gcc -o pommy `pkg-config --cflags --libs libnotify ncurses` pommy.c
 
-int main() {
+// globals
+int timer[] = {2, 5, 0, 0};
+
+int digit[] = {
+  0x7B6F,
+  0x2492,
+  0x73E7,
+  0x7ECF,
+  0x5BC9,
+  0x79CF,
+  0x79EF,
+  0x7249,
+  0x7BEF,
+  0x7BC9
+};
+
+int stop = 0;
+
+
+int main(int argc, char ** argv) {
   // ncurses initialization
   initscr();
   raw();
