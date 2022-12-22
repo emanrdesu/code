2bd5e7733f93efe1837e1239c950e4be237a334a
emanrdesu <eperezpozo@knights.ucf.edu>
2022-12-21 21:21:31 -0500

    Add command line processing

--- c/pommy.c
+++ c/pommy.c
@@ -2,6 +2,7 @@
 #include <ncurses.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <string.h>
 #include <libnotify/notify.h>
 
 // compile command:
@@ -11,7 +12,11 @@
 // e.g. pommy 30 5 15 (default = 25 5 20)
 
 // globals
-int timer[] = {2, 5, 0, 0};
+
+enum timer_t{SESSION, BREAK, EXTENDED};
+
+int  * timer;
+char * timer_default[] = {"25", "5", "20"};
 
 int digit[] = {
   0x7B6F,
@@ -32,10 +37,7 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void * draw_worker(void * vargp) {
   while(1) {
-    if(stop) {
-      pthread_mutex_unlock(&mutex);
-      return NULL;
-    }
+    if(stop) return NULL;
 
     // draw digits
     sleep(1);
@@ -54,13 +56,76 @@ void set_stop(int new) {
 }
 
 
+int ctoi(char c) {
+  switch(c) {
+  case '1': return 1; break;
+  case '2': return 2; break;
+  case '3': return 3; break;
+  case '4': return 4; break;
+  case '5': return 5; break;
+  case '6': return 6; break;
+  case '7': return 7; break;
+  case '8': return 8; break;
+  case '9': return 9; break;
+  }
+}
+
+int digitp(char c) {
+  return c >= 48 && c <= 57;
+}
+
+int integerp(char * str) {
+  for(int i = 0; str[i] != '\0'; i++)
+    if(!digitp(str[i])) return 0;
+
+  return 1;
+}
+
+int * get_digits(char * number) {
+  int size = strlen(number) + 1;
+  int * digits = malloc(size * sizeof(int));
+
+  digits[0] = size;
+
+  for(int i = 1; i < size; i++)
+    digits[i] = ctoi(number[i]);
+
+  return digits;
+}
+
+
 int main(int argc, char ** argv) {
-  // ncurses initialization
+
+  // command line processing
+
+  if(argc > 4) {
+    fprintf(stderr, "Error: pommy takes 3 arguments max.\n");
+    return 1;
+  }
+
+  for(int i = 1; i < argc; i++)
+    if(!integerp(argv[i])) {
+      fprintf(stderr, "Error: '%s' is not an integer\n", argv[i]);
+      return 1;
+    }
+
+  if (argc >= 2)
+    timer_default[SESSION] = argv[1];
+
+  if (argc >= 3)
+    timer_default[BREAK] = argv[2];
+
+  if (argc == 4)
+    timer_default[EXTENDED] = argv[3];
+
+
+  // initialization
+  // ncurses
   initscr();
   raw();
   noecho();
 
-  // libnotify initialization
+  // libnotify
   notify_init ("pommy");
 
 
